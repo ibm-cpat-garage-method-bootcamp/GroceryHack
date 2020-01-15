1a62bb75ac1748789b2ed053e70dbd5b411c75f6
Trevor Cannon <trevor.cannon@ibm.com>
2020-01-15 10:53:12 -0600

    created a pantry list and form for adding items
    Co-authored-by: Whitney Lee <whitney.lee@ibm.com>

--- client/src/components/UIShellBody.jsx
+++ client/src/components/UIShellBody.jsx
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import SimpleList from "../pattern-components/SimpleList";
 import BasicPage from "../pattern-components/BasicPage";
 import "../pattern-components/patterns.scss";
+import PantryList from "./pantry-list/PantryList";
+import AddEditForm from "./edit-list/AddEditForm"
 
 class UIShellBody extends Component {
   components = {
     "Simple List": SimpleList,
-    "Basic Page": BasicPage
+    "Basic Page": BasicPage,
+    "Pantry List": PantryList,
+    "Add Edit Form": AddEditForm
   };
-  defaultComponent = "Basic Page";
+  defaultComponent = "Add Edit Form";
 
   render() {
     let curScreen = this.defaultComponent;
